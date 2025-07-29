089c4c015b203d2910f8de678ca2aaa3091116dd
a9eel99 <143273837+a9eel99@users.noreply.github.com>
2025-07-29 18:55:44 +0300

    Create Join an Array into a String.js

---
+++ Join an Array into a String.js
@@ -0,0 +1,6 @@
+function arrayToString(arr) {
+return arr.join('');
+}
+console.log(arrayToString(['h', 'e', 'l', 'l', 'o'])); 
+console.log(arrayToString(['J', 'a', 'v', 'a', 'S', 'c', 'r', 'i', 'p',
+'t'])); 
